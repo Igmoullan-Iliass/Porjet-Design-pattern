bd648531004fcf5bfa4f53eb03a679dac0a03a3f
saifmarouane <marouansaif3@gmail.com>
2022-12-28 15:58:54 +0100

    commithh

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//hh
 // Route::get('/login', function () {
 //     return view('login');
 Route::get('/', function () {
