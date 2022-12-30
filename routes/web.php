04733c555c5e5bc6df688782d82852600649ca0d
saifmarouane <marouansaif3@gmail.com>
2022-12-30 21:28:57 +0100

    ss

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/admin/acceuille', function () {
-    return view('layoutAdmin.navbar');
+    return view('admin.admin');
 });
 
 // Route::get('/', function () {
