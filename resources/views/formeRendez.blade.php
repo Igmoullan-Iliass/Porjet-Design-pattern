b509f754bdc0438558eade0ba5ac5077d1ea579c
chaima-bd <99513015+chaima-bd@users.noreply.github.com>
2023-01-08 11:27:17 +0100

    commite chaimaa

--- resources/views/formeRendez.blade.php
+++ resources/views/formeRendez.blade.php
@@ -8,7 +8,7 @@
 
   <!-- Modal -->
   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
